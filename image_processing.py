062e5d4fd38511ac957b3708bb62558711bd9464
Albert Guo <guozhaonan@gmail.com>
2016-04-02 23:45:48 -0400

    api for processing images, writing to arduino

--- image_processing.py
+++ image_processing.py
@@ -2,64 +2,60 @@
 from PIL import Image
 import numpy
 import math
+import serial
 numpy.set_printoptions(threshold=numpy.nan)
 
-#Open Image
-im = Image.open("test.png")
+def process_image(file_path):
+    #Open Image
+    im = Image.open(file_path)
 
-#Width and Height
-size = im.size
-width = size[0]
-height = size[1]
-print(width)
-print(height)
+    #Width and Height
+    size = im.size
+    width = size[0]
+    height = size[1]
 
-#For thumbnailing if image is too big
-dimensions = (250, 250)
-if (width >= 250 or height >= 250):
-    im.thumbnail(dimensions)
+    #For thumbnailing if image is too big
+    dimensions = (288, 288)
+    if (width >= 288 or height >= 288):
+        im.thumbnail(dimensions)
 
-#Convert image to Grayscale
-im = im.convert('L')
+    #Convert image to Grayscale
+    im = im.convert('L')
 
-#Width and Height
-size = im.size
-width = size[0]
-height  = size[1]
+    #Width and Height
+    size = im.size
+    width = size[0]
+    height  = size[1]
 
-print size
-#Pading Code
-# if (height < 250):
-#     padding-ud = (250 - height)/2
-# if (width < 250):
-#     padding-lr = (250 - width)/2
-#Convert Grayscale
-y = numpy.asarray(im.getdata(),dtype=numpy.float64).reshape((im.size[1],im.size[0]))
+    #Padding Code
+    y = numpy.asarray(im.getdata(),dtype=numpy.float64).reshape((im.size[1],im.size[0]))
 
-new_array = numpy.empty((y.shape[0], y.shape[1]), None)
-for i in range(len(y)):
-    for j in range(len(y[i])):
-        if y[i][j] >= 127.5:
-            new_array[i][j] = 255
-        else:
-            new_array[i][j] = 0
-new_image = Image.fromarray(new_array, None)
+    new_array = numpy.empty((y.shape[0], y.shape[1]), None)
+    for i in range(len(y)):
+        for j in range(len(y[i])):
+            if y[i][j] >= 127.5:
+                new_array[i][j] = 0
+            else:
+                new_array[i][j] = 1
+    new_image = Image.fromarray(new_array, None)
 
 
-start_width = int(math.ceil((float(288 - width)/2)))
-end_width =  int(math.ceil((float(288 - width)/2)) + width)
+    start_width = int(math.ceil((float(288 - width)/2)))
+    end_width =  int(math.ceil((float(288 - width)/2)) + width)
 
-start_height = int(math.ceil((float(288 - height)/2)))
-end_height =  int(math.ceil((float(288 - height)/2))) + height
-# Initialize Toast Array
-tup = (288, 288)
-toast_array = numpy.zeros(tup)
-print start_width, end_width, start_height, end_height
-for i in range(start_height, end_height):
-    for j in range(start_width, end_width):
-        print i-start_height, j-start_width
-        toast_array[i][j] = new_array[i-start_height][j-start_width]
+    start_height = int(math.ceil((float(288 - height)/2)))
+    end_height =  int(math.ceil((float(288 - height)/2))) + height
+    # Initialize Toast Array
+    tup = (288, 288)
+    toast_array = numpy.zeros(tup)
+    for i in range(start_height, end_height):
+        for j in range(start_width, end_width):
+            toast_array[i][j] = new_array[i-start_height][j-start_width]
 
+    return toast_array
 
-new_image = Image.fromarray(toast_array, None)
-new_image.show()
+def write_to_serial(_array):
+    ser = serial.Serial('dev/tty.usbserial', 9600)
+    for i in range(0, 288):
+        for j in range(0, 288):
+            ser.write("" + i + "," + j + "," + _array[i][j])
